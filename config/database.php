c86fe22b2374b39a614e05d4c2b997e54b90b6a7
Skilerperez44 <skilerperez44@gmail.com>
2026-07-30 21:34:31 -0600

    MiBus corre en el Localhost y en infinityFree sin tener que modificar manualmente los cambios de prueba local o en otro dispositivo

--- config/database.php
+++ config/database.php
@@ -1,9 +1,18 @@
 <?php
 
-$host = "sql211.infinityfree.com";
-$usuario = "if0_42523829";
-$contrasena = "JustoSiu18";
-$basedatos = "if0_42523829_localizador";
+$esLocal = (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false);
+
+if ($esLocal) {
+    $host = "localhost";
+    $usuario = "root";
+    $contrasena = "";
+    $basedatos = "localizador_buses";
+} else {
+    $host = "sql211.infinityfree.com";
+    $usuario = "if0_42523829";
+    $contrasena = "JustoSiu18";
+    $basedatos = "if0_42523829_localizador";
+}
 
 $conexion = new mysqli($host, $usuario, $contrasena, $basedatos);
 
